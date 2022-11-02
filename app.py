820cc188293f36095d8ac75e98cd1328574234cf
Pabloanm <pabloanm@yahoo.com>
2022-11-02 00:01:24 -0300

    endpoint getProduct

--- app.py
+++ app.py
@@ -10,10 +10,17 @@ def ping():
     #return "Follow The White Rabbit"
     return jsonify({"message":"pong!"})
 
-@app.route('/products') 
+@app.route('/products') #GET default method 
 def getProducts():
     #return jsonify(products)
-    return jsonify({"products":products})
+    #return jsonify({"products":products})
+    return jsonify({"products":products, "message": "Product's List"})
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    print(product_name) #print in terminal
+    return 'received' #print in browser
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000) #python app.py to run this server
