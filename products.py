4742ae2e7f1cec298a7d8d2af27de6e0410172d3
Pabloanm <pabloanm@yahoo.com>
2022-11-01 22:29:02 -0300

    Initial Commit

---
+++ products.py
@@ -0,0 +1,6 @@
+'''Test Database'''
+products = [
+    {"name": "laptop", "price": 800, "quantity":4},
+    {"name": "monitor", "price": 40, "quantity":10},
+    {"name": "monitor", "price": 450, "quantity":3},
+]
