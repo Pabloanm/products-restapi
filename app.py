5bfb231ca56145f888eaee56bf58894a4959dd89
Pabloanm <pabloanm@yahoo.com>
2022-11-01 23:45:21 -0300

    endpoint products

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 '''Flask'''
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__) #my server app
 
 from products import products #import product list form products.py
 
-@app.route('/ping') #endpoint
+@app.route('/ping', methods=['GET']) #endpoint / GET default method 
 def ping(): 
-    return "Follow The White Rabbit"
+    #return "Follow The White Rabbit"
+    return jsonify({"message":"pong!"})
+
+@app.route('/products') 
+def getProducts():
+    #return jsonify(products)
+    return jsonify({"products":products})
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000) #python app.py to run this server
