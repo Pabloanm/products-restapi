37546d5fe95f364b4572bd8777a9f3e28e2e3fee
Pabloanm <pabloanm@yahoo.com>
2022-11-01 23:18:14 -0300

    testing route

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 '''Flask'''
+from flask import Flask
+
+app = Flask(__name__) #my server app
+
+from products import products #import product list form products.py
+
+@app.route('/ping') #endpoint
+def ping(): 
+    return "Follow The White Rabbit"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000) #python app.py to run this server
+
